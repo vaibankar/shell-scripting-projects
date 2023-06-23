ef92e5b8333994b1a1fd4524b1e1a40c7d36c0a2
vaibankar <113019542+vaibankar@users.noreply.github.com>
2023-06-23 14:46:07 +0530

    Update s3-notification-triggers.sh

--- aws-event-triggering/s3-notification-triggers.sh
+++ aws-event-triggering/s3-notification-triggers.sh
@@ -6,14 +6,14 @@ set -x
 aws_account_id=$(aws sts get-caller-identity --query 'Account' --output text)
 
 # Print the AWS account ID from the variable
-echo "AWS Account ID: $aws_account_id"
+echo "AWS Account ID: $723856713242"
 
 # Set AWS region and bucket name
 aws_region="us-east-1"
-bucket_name="abhishek-ultimate-bucket"
+bucket_name="vaibhav-ultimate-bucket"
 lambda_func_name="s3-lambda-function"
 role_name="s3-lambda-sns"
-email_address="zyz@gmail.com"
+email_address="linuxdocs111@gmail.com"
 
 # Create IAM Role for the project
 role_response=$(aws iam create-role --role-name s3-lambda-sns --assume-role-policy-document '{
